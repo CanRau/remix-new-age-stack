b7acb67413755d3e824abda31c1fec2f2b943da6
CanRau <cansrau@gmail.com>
2022-06-21 17:34:35 -0500

    feat: use oak server, add x-fly-region response header

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,8 @@
-import type { EntryContext } from "@remix-run/deno";
+import type {
+  DataFunctionArgs,
+  EntryContext,
+  HandleDataRequestFunction,
+} from "@remix-run/deno/index.ts";
 import { RemixServer } from "@remix-run/react";
 import * as React from "react";
 import { renderToString } from "react-dom/server";
@@ -15,8 +19,21 @@ export default function handleRequest(
 
   responseHeaders.set("Content-Type", "text/html");
 
+  responseHeaders.set("x-fly-region", Deno.env.get("FLY_REGION") ?? "unknown");
+  // responseHeaders.set("Strict-Transport-Security", `max-age=${60 * 60 * 24 * 365 * 100}`);
+
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
   });
 }
+
+export const handleDataRequest: HandleDataRequestFunction = (
+  response: Response,
+  // same args that get passed to the action or loader that was called
+  _args: DataFunctionArgs,
+) => {
+  response.headers.set("x-fly-region", Deno.env.get("FLY_REGION") ?? "unknown");
+  // response.headers.set("Strict-Transport-Security", `max-age=${60 * 60 * 24 * 365 * 100}`);
+  return response;
+};
--- server.ts
+++ server.ts
@@ -1,4 +1,8 @@
-import { serve } from "https://deno.land/std@0.144.0/http/server.ts";
+import {
+  Application,
+  type Context as OakContext,
+  Router,
+} from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import { createRequestHandlerWithStaticFiles } from "@remix-run/deno/server.ts";
 // Import path interpreted by the Remix compiler
 import * as build from "@remix-run/dev/server-build";
@@ -9,6 +13,44 @@ const remixHandler = createRequestHandlerWithStaticFiles({
   getLoadContext: () => ({}),
 });
 
+const app = new Application();
+const router = new Router();
+
+app.addEventListener(
+  "error",
+  (error) => console.error("CATHED ERROR", error, "END CATCHED ERROR"),
+);
+
+app.addEventListener(
+  "listen",
+  ({ hostname, port, secure }) =>
+    console.log(`Listening on http://${hostname}:${port}`, { secure }),
+);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(wrapOakRequest(remixHandler));
+
 const port = Number(Deno.env.get("PORT")) || 8000;
-console.log(`Listening on http://localhost:${port}`);
-serve(remixHandler, { port });
+// @note: esbuild doesn't support top-level await at the moment [evanw/esbuild/#253](https://github.com/evanw/esbuild/issues/253)
+app.listen({ port }).then(console.log).catch(console.error);
+
+// @from: https://github.com/oakserver/oak/issues/533#issuecomment-1162372606
+export function wrapOakRequest(
+  fn: (request: Request) => Promise<Response>,
+) {
+  return async (ctx: OakContext) => {
+    const req = new Request(ctx.request.url.toString(), {
+      body: ctx.request.originalRequest.getBody().body,
+      headers: ctx.request.headers,
+      method: ctx.request.method,
+    });
+
+    const response = await fn(req);
+
+    ctx.response.status = response.status;
+    ctx.response.headers = response.headers;
+    ctx.response.body = response.body;
+  };
+}
